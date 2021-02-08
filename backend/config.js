d32651921d0b3f94bfa8f9faaf2482e2fadeca0b
bhupendrabanothe <bhupendrabanothe@gmail.com>
2021-02-08 12:17:13 +0530

    fix: database config file

--- backend/config.js
+++ backend/config.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
+const dotenv =  require('dotenv').config();
 
-const Url = "mongodb+srv://dbuser:dbpass@cluster0.ahg4r.mongodb.net/ecom?retryWrites=true&w=majority";
+const Url = `mongodb+srv://${process.env.DATABASEUSER}:${process.env.DATABASEPASS}@cluster0.ahg4r.mongodb.net/${process.env.DATABASENAME}?retryWrites=true&w=majority`;
 
 const connectDB = async() => {
     await mongoose.connect(Url, { useUnifiedTopology: true, useNewUrlParser: true });
